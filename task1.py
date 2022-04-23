51afcfa9f335184eec26fd913c13760d6d249ec8
Dilnura <dilnuradusha@gmail.com>
2022-04-23 17:22:47 +0500

    added file

---
+++ task1.py
@@ -0,0 +1,14 @@
+# def even_numbers():
+#     count1 = ([5, 4, 1, 2, -1, -2, 1, 0, 1, 0, 0, 0, 0,])
+#     print(count1[1], count1[3], count1[6], count1[8], count1[9], count1[11])
+
+# even_numbers()
+
+#2
+def get_century():
+    year = int(input('Введите год: '))
+    if year % 400 == 0:
+        print()
+
+
+
